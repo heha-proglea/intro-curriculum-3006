64d6b3aebd15d57ea9308d306966aa8bd5ae1fc1
heha-proglea <heha965+proglea@gmail.com>
2018-10-19 09:34:02 +0000

    add: add comments

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,13 @@
 'use strict';
+
+// 無名関数をモジュールとして設定する(関数をモジュール化した)(←module.exportsオブジェクトのプロパティとして関数を登録してる)
 module.exports = (robot) => {
+	// HubotのAPI利用方法に則って記述する
 	robot.hear(/hello>/i, (msg) => {
+    // 正規表現で、大文字小文字問わずhello>という文字にマッチした場合→次の無名関数を実行する
 		const username = msg.message.user.name;
+		// msgオブジェクトから(slackの?)ユーザ名を取得する(※ちなみにVMのshell上で実行するとユーザ名はShellとなる)
 		msg.send('Hello, ' + username);
+		// msgオブジェクトのsendメソッドを用いて「発言」する
 	});
 };
